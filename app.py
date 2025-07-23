ee197107559a6050c9d498dd605b674ff72c7c91
Manvir Singh <168892034+manvirsingh1716@users.noreply.github.com>
2025-07-23 12:43:11 +0530

    correct synatx

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ CORS(app)
 
 model = joblib.load("nifty_lr_model.pkl")
 raw_features = joblib.load("features.pkl")
-features = [f[0] for f in raw_features]
+# features = [f[0] for f in raw_features]
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.get_json()
-        values = [float(data[feature]) for feature in features]
+        values = [float(data[feature]) for feature in raw_features]
         prediction = float(model.predict([values])[0])
         return jsonify({"prediction": round(prediction, 2)})
     except Exception as e:
@@ -23,7 +23,7 @@ def predict():
 
 @app.route("/features", methods=["GET"])
 def get_features():
-    return jsonify({"features": features})
+    return jsonify({"features": raw_features})
 
 if __name__ == "__main__":
     app.run(debug=True)
